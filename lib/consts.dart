a9d75919a1ac144316589ed4d1e6c8588856d758
samiksha <samikshad603@gmail.com>
2024-10-24 18:08:04 -0400

    finished code

--- lib/consts.dart
+++ lib/consts.dart
@@ -3,6 +3,7 @@ const String OPENAI_API_KEY = 'sk-proj-ttxUwVvu29WSbeaT9PTbVioexAiAxDjuIy8jtTmaG
 const String GOOGLE_API_KEY = 'AIzaSyAUzZ77cvmLBxHK9RjVq5A1IV95md2DOow';
 const String GEMINI_API_KEY = 'AIzaSyChRC06k5aJf89QAgal5jHHPNGZco6Xg7A';
 const String OPENWEATHER_API_KEY = '3585006e8d837437f3af6bf8b7fb4cda';
+const String HUGGINGFACE_TOKEN = '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';
 const Color softBeige = Color(0xFFF5F0E1);
 const Color mutedGreen = Color(0xFFA8C686);
 const Color warmBrown = Color(0xFF8C6D5B);
